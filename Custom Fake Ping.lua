20a0841ca9d4f1a55af0fecc2ff6636cd1a5fd9a
Robonyantame <34773729+bakatame@users.noreply.github.com>
2022-10-29 04:32:43 +0800

    Update Custom Fake Ping.lua

--- Custom Fake Ping.lua
+++ Custom Fake Ping.lua
@@ -7,8 +7,7 @@ menu.add_slider("Fake ping value", 0, 0, 200)
 hooks.add_hook("on_draw", function ()
 
     local local_player = engine.get_local_player()
-	if not local_player or not engine.is_ingame() and not engine.is_connected() or local_player:get_netvar_int("m_lifeState") ~= 0 or menu.get_keybind_mode("Fake ping key") == 3 or not menu.get_keybind("Fake ping key") then return end
-    
+    if not local_player or not engine.is_ingame() and not engine.is_connected() or local_player:get_netvar_int("m_lifeState") ~= 0 or menu.get_keybind_mode("Fake ping key") == 3 or not menu.get_keybind("Fake ping key") then return end
     local ping = player_resource.get_netvar_int(local_player:get_index(), "m_iPing")
 
     local text = "Fake Ping: " .. ping
